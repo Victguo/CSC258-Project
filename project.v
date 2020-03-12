c091fc9d6425aa6ea8ff22cd856973d8f8b1dcaf
victguo <victorguo_123@outlook.com>
2020-03-12 15:55:00 -0400

    WIP debug

--- project.v
+++ project.v
@@ -2,7 +2,7 @@
 // KEY[0] is the reset switch
 
 // ADDITIONAL TODO: Refactor the FSM code so its not just one whole module where the game runs?
-module Proj(
+module proj(
 		CLOCK_50,						//	On Board 50 MHz
 		// Your inputs and outputs here
         KEY,
@@ -78,7 +78,8 @@ module Proj(
 	reg lower_blocks;
 	reg timer_triggered;
 	reg done_lowering;
-	
+	reg yeet;
+
 	assign LEDR[5:0] = state;
 	 
 	// States in the FSM
@@ -116,21 +117,22 @@ module Proj(
 		);
 
 	assign LEDR[7] = ((b_y_direction) && (b_y > p_y - 8'd1) && (b_y < p_y + 8'd2) && (b_x >= p_x) && (b_x <= p_x + 8'd8));
-
+	assign LEDR[17] = timer_triggered;
 	always@(posedge CLOCK_50)
     	begin
 			colour = 3'b000;
 			x = 8'b00000000;
 			y = 8'b00000000;
-			counter = 2'b00;
-			lower_blocks = 1'b0;
-			timer_triggered = 1'b0;
-			done_lowering = 1'b0;
 			if (~KEY[0]) 
 				state = RESET_BLACK;
 			// FSM
 			case (state)
 				RESET_BLACK: begin
+					counter = 2'b00;
+					lower_blocks = 1'b0;
+					timer_triggered = 1'b0;
+					done_lowering = 1'b0;
+					yeet = 1'b0;
 					if (draw_counter < 17'b10000000000000000) begin
 						x = draw_counter[7:0];
 						y = draw_counter[16:8];
@@ -196,20 +198,20 @@ module Proj(
 				end
 				IDLE: begin
 					// Frame is the rateDivider, when the rate divider hits 0, begin updating the game
-					lower_blocks = 1'b0;
 					if (frame)
 						state = ERASE_PADDLE;
+				end
+				ERASE_PADDLE: begin
+					lower_blocks = 1'b0;
 					if (timer_triggered) begin
 						lower_blocks = 1'b1;
 						counter = counter - 1'b1;
 
-						if(counter == 1'b0) begin
+						if(counter == 2'b00) begin
 							timer_triggered = 1'b0;
 							done_lowering = 1'b1;
 						end
 					end
-				end
-				ERASE_PADDLE: begin
 					if (draw_counter < 6'b100000) begin
 						x = p_x + draw_counter[3:0];
 						y = p_y + draw_counter[4];
@@ -223,7 +225,7 @@ module Proj(
 				UPDATE_PADDLE: begin
 					if (~KEY[1] && p_x < 8'd144)
 						p_x = p_x + 1'b1;
-					if (~KEY[2] && p_x > 8'd0)
+					if (~KEY[3] && p_x > 8'd0)
 						p_x = p_x - 1'b1;
 
 					state = DRAW_PADDLE;
@@ -260,15 +262,23 @@ module Proj(
 					// Screen is 160px wide (x), therefore reverse x's direction if we hit a boundary
 					if ((b_x == 8'd0) || (b_x == 8'd160)) 
 						b_x_direction = ~b_x_direction;
+
+					if (b_y == 8'd0)
+						b_y_direction = ~b_y_direction;
 					
 					// Check if we hit the paddle, this is where we need to increment the counter
-					if ((b_y == 8'd0) || ((b_y_direction) && (b_y > p_y - 8'd1) && (b_y < p_y + 8'd2) && (b_x >= p_x) && (b_x <= p_x + 8'd15))) begin
+					if ((b_y_direction) && (b_y > p_y - 8'd1) && (b_y < p_y + 8'd2) && (b_x >= p_x) && (b_x <= p_x + 8'd15)) begin
 						b_y_direction = ~b_y_direction;
 
-						if((counter < 2'b11) && (timer_triggered == 1'b0) && (!done_lowering))
+						if((counter < 2'b11) && (timer_triggered == 1'b0) && (done_lowering == 1'b0)) begin
 							counter = counter + 1'b1;
+							yeet = 1'b1;
+							if(counter == 2'b11)
+								timer_triggered = 1'b1;
+						end
 						else begin
 							timer_triggered = 1'b1;
+							yeet = 1'b1;
 						end
 					end
 					// If we hit the bottom of the screen, the ball is dead, else continue drawing the ball
@@ -296,7 +306,7 @@ module Proj(
 					end
 					// Trying 10 pixels for now
 					if(lower_blocks == 1'b1)
-						bl_1_y = bl_1_y + 4'b1010;
+						bl_1_y = bl_1_y + 5'b10100;
 
 					state = DRAW_BLOCK_1;
 				end
@@ -320,7 +330,7 @@ module Proj(
 					end
 					// Trying 10 pixels for now
 					if(lower_blocks == 1'b1)
-						bl_2_y = bl_2_y + 4'b1010;
+						bl_2_y = bl_2_y + 5'b10100;
 					state = DRAW_BLOCK_2;
 				end
 				DRAW_BLOCK_2: begin
